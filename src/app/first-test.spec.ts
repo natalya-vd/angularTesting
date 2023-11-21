3805cc7ddaa4ffbb11cd78ecc807cde8cfb06814
Natalya <natasha-vd@yandex.ru>
2023-11-21 17:27:46 +0300

    first test

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,19 @@
+describe('First Test', () => {
+  let testVariable: any
+
+  // Выполняется перед каждым запуском
+  beforeEach(() => {
+    testVariable = {}
+  })
+
+  it('should return true if a is true', () => {
+    // Инициализация данных
+    testVariable.a = false
+
+    // Проверка логики
+    testVariable.a = true
+
+    // Утверждение
+    expect(testVariable.a).toBe(false)
+  })
+})
