dbf2308d7c05eb37152a360608da52317590f0c1
Natalya <natasha-vd@yandex.ru>
2023-11-22 10:19:09 +0300

    LoggerService

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,15 +1,18 @@
+import { LoggerService } from "../logger/logger.service"
 import { CalculatorService } from "./calculator.service"
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    const calculator = new CalculatorService()
+    const loggerService = new LoggerService()
+    const calculator = new CalculatorService(loggerService)
     const res = calculator.add(2, 2)
 
     expect(res).toBe(4)
   })
 
   it('should subtract two numbers', () => {
-    const calculator = new CalculatorService()
+    const loggerService = new LoggerService()
+    const calculator = new CalculatorService(loggerService)
     const res = calculator.subtract(2, 2)
 
     expect(res).toBe(0)
