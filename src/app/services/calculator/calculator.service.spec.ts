80daa8e07855e29161cd9ae4f23ffecdf0079e20
Natalya <natasha-vd@yandex.ru>
2023-11-22 10:41:26 +0300

    spyOn & spyObj

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -3,18 +3,23 @@ import { CalculatorService } from "./calculator.service"
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    const loggerService = new LoggerService()
-    const calculator = new CalculatorService(loggerService)
+    const mockLoggerService = jasmine.createSpyObj('LoggerService', ['log'])  // Создаем объект-шпион
+    // spyOn(loggerService, 'log') // Следит за методом, но метод не вызывается в CalculatorService
+
+    const calculator = new CalculatorService(mockLoggerService)
     const res = calculator.add(2, 2)
 
     expect(res).toBe(4)
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
   })
 
   it('should subtract two numbers', () => {
-    const loggerService = new LoggerService()
-    const calculator = new CalculatorService(loggerService)
+    const mockLoggerService = jasmine.createSpyObj('LoggerService', ['log'])
+
+    const calculator = new CalculatorService(mockLoggerService)
     const res = calculator.subtract(2, 2)
 
     expect(res).toBe(0)
+    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
   })
 })
