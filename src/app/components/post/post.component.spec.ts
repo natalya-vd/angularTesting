ae32a335f1b1e6ea17cfdb323ab726bb9d1119f3
Natalya <natasha-vd@yandex.ru>
2023-11-24 19:24:56 +0300

    PostComponent: TestBed create component

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,10 +1,26 @@
+import { first } from "rxjs"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+
 import { Post } from "src/app/models/post"
 import { PostComponent } from "./post.component"
-import { first } from "rxjs"
 
 describe('PostComponent', () => {
+  let fixture: ComponentFixture<PostComponent>
+  let component: PostComponent
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PostComponent]
+    })
+    fixture = TestBed.createComponent(PostComponent)
+    component = fixture.componentInstance
+  })
+
+  it('should create post component using TestBed', () => {
+    expect(component).toBeDefined()
+  })
+
   it('should raise and event when the delete post is clicked', () => {
-    const component = new PostComponent()
     const post: Post = {
       id: 1,
       title: 'title 1',
