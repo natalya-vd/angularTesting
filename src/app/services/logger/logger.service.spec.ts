cbab64ff37cf91ff5724dde6bc2a4fbc3407fef4
Natalya <natasha-vd@yandex.ru>
2023-11-24 14:43:57 +0300

    test for LoggerService

--- src/app/services/logger/logger.service.spec.ts
+++ src/app/services/logger/logger.service.spec.ts
@@ -1,16 +1,30 @@
-// import { TestBed } from '@angular/core/testing';
+import { LoggerService } from "./logger.service"
 
-// import { LoggerService } from './logger.service';
+describe('LoggerService', () => {
+  let service: LoggerService
 
-// describe('LoggerService', () => {
-//   let service: LoggerService;
+  beforeEach(() => {
+    service = new LoggerService()
+  })
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(LoggerService);
-//   });
+  it('should not have any messages at starting', () => {
+    const count = service.messages.length
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+    expect(count).toBe(0)
+  })
+
+  it('should add the message when log is called', () => {
+    service.log('message')
+
+    expect(service.messages.length).toBe(1)
+    expect(service.messages).toContain('message')
+  })
+
+  it('should clear all the messages when clear is called', () => {
+    service.log('message')
+
+    service.clear()
+
+    expect(service.messages.length).toBe(0)
+  })
+})
