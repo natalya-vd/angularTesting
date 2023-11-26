a6a074b2d3e5e35fa58b65142d0afd1266366337
Natalya <natasha-vd@yandex.ru>
2023-11-26 11:31:00 +0300

    PostComponent:nativeElement & detectChanges

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,5 +1,6 @@
 import { first } from "rxjs"
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { NO_ERRORS_SCHEMA } from "@angular/core"
 
 import { Post } from "src/app/models/post"
 import { PostComponent } from "./post.component"
@@ -10,7 +11,8 @@ describe('PostComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PostComponent]
+      declarations: [PostComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     fixture = TestBed.createComponent(PostComponent)
     component = fixture.componentInstance
@@ -18,6 +20,22 @@ describe('PostComponent', () => {
 
   it('should create post component using TestBed', () => {
     expect(component).toBeDefined()
+  })
+
+  it('should render the post title in the anchor element', () => {
+    const post: Post = {
+      id: 1,
+      title: 'title 1',
+      body: 'body 1'
+    }
+
+    component.post = post
+    // Чтобы angular обнаружил изменения в компоненте (присвоили post) нужно вызвать fixture.detectChanges()
+    fixture.detectChanges()
+    const postElement: HTMLElement = fixture.nativeElement
+    const a = postElement.querySelector('a')
+
+    expect(a?.textContent).toContain(post.title)
   })
 
   it('should raise and event when the delete post is clicked', () => {
