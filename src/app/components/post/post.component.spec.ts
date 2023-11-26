de3619fdbb64047a6f8b9e4c2654705eea650ae7
Natalya <natasha-vd@yandex.ru>
2023-11-26 11:41:10 +0300

    PostComponent: debugElement

--- src/app/components/post/post.component.spec.ts
+++ src/app/components/post/post.component.spec.ts
@@ -1,9 +1,10 @@
 import { first } from "rxjs"
 import { ComponentFixture, TestBed } from "@angular/core/testing"
-import { NO_ERRORS_SCHEMA } from "@angular/core"
+import { DebugElement, NO_ERRORS_SCHEMA } from "@angular/core"
 
 import { Post } from "src/app/models/post"
 import { PostComponent } from "./post.component"
+import { By } from "@angular/platform-browser"
 
 describe('PostComponent', () => {
   let fixture: ComponentFixture<PostComponent>
@@ -36,6 +37,21 @@ describe('PostComponent', () => {
     const a = postElement.querySelector('a')
 
     expect(a?.textContent).toContain(post.title)
+  })
+
+  it('should render the post title in the anchor element using debug element', () => {
+    const post: Post = {
+      id: 1,
+      title: 'title 1',
+      body: 'body 1'
+    }
+
+    component.post = post
+    fixture.detectChanges()
+    const postDebugElement: DebugElement = fixture.debugElement
+    const aElement: HTMLElement = postDebugElement.query(By.css('a')).nativeElement
+
+    expect(aElement.textContent).toContain(post.title)
   })
 
   it('should raise and event when the delete post is clicked', () => {
