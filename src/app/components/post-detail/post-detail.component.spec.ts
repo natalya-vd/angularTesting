b7a3018d361253cc9de76d79472f6a6d08e3bfea
Natalya <natasha-vd@yandex.ru>
2023-11-27 15:37:12 +0300

    PostDetailComponent: render title in h2

--- src/app/components/post-detail/post-detail.component.spec.ts
+++ src/app/components/post-detail/post-detail.component.spec.ts
@@ -4,11 +4,23 @@ import { Location } from "@angular/common"
 
 import { PostDetailComponent } from "./post-detail.component"
 import { PostService } from "src/app/services/post/post.service"
+import { of } from "rxjs"
+import { Post } from "src/app/models/post"
+import { By } from "@angular/platform-browser"
+import { FormsModule } from "@angular/forms"
 
 describe('PostDetailComponent', () => {
   let fixture: ComponentFixture<PostDetailComponent>
+  let mockPostService: jasmine.SpyObj<PostService>
+  let post: Post
 
   beforeEach(() => {
+    post = {
+      id: 3,
+      title: 'title 1',
+      body: 'body 1'
+    }
+
     const mockActivatedRoute = {
       snapshot: {
         paramMap: {
@@ -18,18 +30,32 @@ describe('PostDetailComponent', () => {
         }
       }
     }
-    const mockPostService = jasmine.createSpyObj(['getPost', 'updatePost'])
+    mockPostService = jasmine.createSpyObj(['getPost', 'updatePost'])
     const mockLocation = jasmine.createSpyObj(['back'])
 
     TestBed.configureTestingModule({
       declarations: [PostDetailComponent],
+      imports: [FormsModule],
       providers: [
         {provide: Location, useValue: mockLocation},
         {provide: PostService, useValue: mockPostService},
         {provide: ActivatedRoute, useValue: mockActivatedRoute},
-      ]
+      ],
     })
 
     fixture = TestBed.createComponent(PostDetailComponent)
+  })
+
+  it('should render the post title in h2 template', () => {
+    mockPostService.getPost.and.returnValue(
+      of(post)
+    )
+    fixture.detectChanges()
+
+    const element = fixture.nativeElement.querySelector('h2')
+    // const elementDes =fixture.debugElement.query(By.css('h2'))
+    // const element: HTMLElement = elementDes.nativeElement
+
+    expect(element.textContent).toBe(post.title)
   })
 })
