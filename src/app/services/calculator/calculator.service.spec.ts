ac2df1bba5e88a1b088b1b328845ad3def376262
Natalya <natasha-vd@yandex.ru>
2023-11-27 10:52:46 +0300

    CalculatorService: inject services

--- src/app/services/calculator/calculator.service.spec.ts
+++ src/app/services/calculator/calculator.service.spec.ts
@@ -1,27 +1,59 @@
+import { TestBed } from "@angular/core/testing"
 import { CalculatorService } from "./calculator.service"
+import { LoggerService } from "../logger/logger.service"
+
+// Для установки настроек можно создать отдельную функцию и вызывать ее в каждом it, а не устанавливать настройки в beforeEach
+// function setUp() {
+//   const mockLoggerService = jasmine.createSpyObj('LoggerService', ['log'])  // Создаем объект-шпион
+//   // spyOn(loggerService, 'log') // Следит за методом, но метод не вызывается в CalculatorService
+//   TestBed.configureTestingModule({
+//     providers: [
+//       CalculatorService,
+//       {
+//         provide: LoggerService,
+//         useValue: mockLoggerService
+//       }
+//     ]
+//   })
+
+//   const calculator = TestBed.inject(CalculatorService)
+//   const loggerServiceSpy = TestBed.inject(LoggerService) as jasmine.SpyObj<LoggerService>
+
+//   return {calculator, loggerServiceSpy}
+// }
 
 describe('CalculatorService', () => {
-  let mockLoggerService: any
   let calculator: CalculatorService
+  let loggerServiceSpy: jasmine.SpyObj<LoggerService>
 
   beforeEach(() => {
-    mockLoggerService = jasmine.createSpyObj('LoggerService', ['log'])  // Создаем объект-шпион
+    const mockLoggerService = jasmine.createSpyObj('LoggerService', ['log'])  // Создаем объект-шпион
     // spyOn(loggerService, 'log') // Следит за методом, но метод не вызывается в CalculatorService
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {
+          provide: LoggerService,
+          useValue: mockLoggerService
+        }
+      ]
+    })
 
-    calculator = new CalculatorService(mockLoggerService)
+    calculator = TestBed.inject(CalculatorService)
+    loggerServiceSpy = TestBed.inject(LoggerService) as jasmine.SpyObj<LoggerService>
   })
 
   it('should add two numbers', () => {
     const res = calculator.add(2, 2)
 
     expect(res).toBe(4)
-    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
+    expect(loggerServiceSpy.log).toHaveBeenCalledTimes(1)
   })
 
   it('should subtract two numbers', () => {
     const res = calculator.subtract(2, 2)
 
     expect(res).toBe(0)
-    expect(mockLoggerService.log).toHaveBeenCalledTimes(1)
+    expect(loggerServiceSpy.log).toHaveBeenCalledTimes(1)
   })
 })
